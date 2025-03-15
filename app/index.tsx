3257734b6094de9acd34d692d52ff6141d23509b
nchimunyascripts <nchmunya.backlogcode@gmail.com>
2025-03-15 08:01:02 +0200

    renew project files

--- app/index.tsx
+++ app/index.tsx
@@ -1,86 +1,52 @@
-// import Example from './examples/0-Switch';
-// import Example from './examples/1-Stack';
-// export default Example;
-
 import React from 'react';
-import { StatusBar, View } from 'react-native';
-import {
-  createStackNavigator,
-  createSwitchNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Button, Text, View } from 'react-native';
+import { createSwitchNavigator } from 'react-navigation';
 
-import AddContactScreen from './screens/AddContactScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import ContactListScreen from './screens/ContactListScreen';
-import ContactDetailsScreen from './screens/ContactDetailsScreen';
-import LoginScreen from './screens/LoginScreen';
-import contacts from '../scripts/contacts';
-
-const MainStack = createStackNavigator(
-  {
-    ContactList: ContactListScreen,
-    ContactDetails: ContactDetailsScreen,
-    AddContact: AddContactScreen,
-  },
-  {
-    initialRouteName: 'ContactList',
-    navigationOptions: {
-      headerTintColor: '#a41034',
-      headerStyle: {
-        backgroundColor: '#fff',
-      },
-    },
-  },
-);
-
-MainStack.navigationOptions = {
-  tabBarIcon: ({ focused, tintColor }) => (
-    <Ionicons
-      name={`ios-contacts${focused ? '' : '-outline'}`}
-      size={25}
-      color={tintColor}
-    />
-  ),
-};
-
-const MainTabs = createBottomTabNavigator(
-  {
-    Contacts: MainStack,
-    Settings: SettingsScreen,
-  },
-  {
-    tabBarOptions: {
-      activeTintColor: '#a41034',
-    },
-  },
-);
-
-const AppNavigator = createSwitchNavigator({
-  Login: LoginScreen,
-  Main: MainTabs,
-});
-
-export default class App extends React.Component {
-  state = {
-    contacts,
-  };
-
-  addContact = (newContact) => {
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
-  render() {
+class ScreenComponentOne extends React.Component {
+  render(): React.ReactNode {
     return (
-      <AppNavigator
-        screenProps={{
-          contacts: this.state.contacts,
-          addContact: this.addContact,
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          borderColor: 'teal',
+          borderWidth: 20,
         }}
-      />
+      >
+        <Button
+          title="Go to Screen Two"
+          onPress={() => this.props.navigation.navigate('RouteNameTwo')}
+        />
+      </View>
     );
   }
+}
+
+class ScreenComponentTwo extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          borderColor: 'orange',
+          borderWidth: 20,
+        }}
+      >
+        <Button
+          title="Go to Screen one"
+          onPress={() => this.props.navigation.navigate('RouteNameOne')}
+        />
+      </View>
+    );
+  }
+}
+
+const IndexNavigation = createSwitchNavigator({
+  RouteNameOne: ScreenComponentOne,
+  RouteNameTwo: ScreenComponentTwo,
+});
+
+export default function Index() {
+  return <IndexNavigation />;
 }
