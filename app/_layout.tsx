cd9b835703ed8183f91edaff8eeb30d2826246cb
nchimunyascripts <nchmunya.backlogcode@gmail.com>
2025-04-15 02:00:40 +0200

    movie app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from 'expo-router';
+import './globals.css';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="./" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
---
+++ interfaces/interfaces.d.ts
@@ -0,0 +1,23 @@
+interface Movie {
+  title: string;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TrendingMovie {
+  searchTerm: string;
+  movie_id: number;
+  title: string;
+  count: number;
+  poster_url: string;
+}
