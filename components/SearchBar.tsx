a82d0debceed33e90d092f6289486bf358f309b4
nchimunyascripts <nchmunya.backlogcode@gmail.com>
2025-04-25 20:36:25 +0200

    api integration

---
+++ components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import { View, Text, Image, TextInput } from 'react-native';
+import React from 'react';
+import { icons } from '@/constants/icons';
+
+interface Props {
+  placeholder: string
+  onPress?: () => void
+}
+
+const SearchBar = ({placeholder, onPress} : Props) => {
+  return (
+    <View className={"flex-row items-center bg-dark-200 rounded-full px-5 py-4"}>
+      <Image source={icons.search} className={"size-5"}
+             resizeMode={"contain"} tintColor={"#ab8bff"} />
+      <TextInput onPress={onPress} placeholderTextColor={"#a8b5db"} onChangeText={() => {}}
+                 value={""} placeholder={placeholder} className={"flex-1 ml-2 text-white"}  />
+    </View>
+  );
+};
+export default SearchBar;
